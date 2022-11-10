cdf73bcd439e95b489908a810b5c7387f03a3b41
John <kibirigejohn005@gmail.com>
2022-11-10 15:56:09 +0300

    Create many to many relationship between person and book

--- src/person.rb
+++ src/person.rb
@@ -2,14 +2,16 @@ require_relative './nameable'
 require_relative './decorator'
 
 class Person < Nameable
-  attr_accessor :name, :age, :parent_permission
+  attr_accessor :name, :age, :parent_permission, :rentals
   attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
     super()
+    @id = Random.rand(1...100000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   def can_use_services?
--- src/rental.rb
+++ src/rental.rb
@@ -1,6 +1,12 @@
 class Rental 
-    attr_accessor :date 
+    attr_accessor :date, :person, :book
+
     def initialize date 
         @date = date
+        @person = person 
+        @book = book 
+
+        person.rentals << self 
+        book.rentals << self 
     end 
 end
